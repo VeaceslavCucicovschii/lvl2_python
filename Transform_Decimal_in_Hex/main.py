43c52b6cd7785113358ff40815568a962fdf9355
VeaceslavCucicovschii <104380929+VeaceslavCucicovschii@users.noreply.github.com>
2022-10-18 18:17:59 +0300

    Update main.py

--- Transform_Decimal_in_Hex/main.py
+++ Transform_Decimal_in_Hex/main.py
@@ -1,9 +1,11 @@
+from http.client import SWITCHING_PROTOCOLS
 from os import system
 system('cls')
 
 
 def FromDecToHex(id):
     counter = 0
+    result = ''
 
     i = len(id) - 1
     while(id[i] != '-'):
@@ -11,9 +13,27 @@ def FromDecToHex(id):
         i -= 1
 
     dec = int(id[len(id)-counter:])
-    Hex = hex(dec)
 
-    return id[:len(id)-counter] + Hex[2:].upper()
+    while(dec != 0):
+        match dec % 16:
+            case 10:
+                result += 'A'
+            case 11:
+                result += 'B'
+            case 12:
+                result += 'C'
+            case 13:
+                result += 'D'
+            case 14:
+                result += 'E'
+            case 15:
+                result += 'F'
+            case default:
+                result += str(dec % 16)
+        dec //= 16
+
+    result = result[::-1]
+    return f'{id[:len(id)-counter]}{result}'
 
 
 id = "2022-01-10-11-01-01-123456"
